228b174a6a04ad6830bdcf181768879511091b2d
Vinicius Ugoski Bacchieri <vbacch@gmail.com>
2024-02-17 11:28:09 -0300

    initial commit

--- App.js
+++ App.js
@@ -1,20 +1,45 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { Button, Picker, TextInput, View } from 'react-native';
+import * as Speech from 'expo-speech';
 
 export default function App() {
+  const [text, setText] = useState('');
+  const [voices, setVoices] = useState([]);
+  const [selectedVoice, setSelectedVoice] = useState('');
+
+  useEffect(() => {
+    Speech.getAvailableVoicesAsync().then(availableVoices => {
+      setVoices(availableVoices);
+      setSelectedVoice(availableVoices[0].id);
+    });
+  }, []);
+
+  const startReading = () => {
+    Speech.speak(text, { voice: selectedVoice });
+  };
+
+  const pauseReading = () => {
+    Speech.pause();
+  };
+
+  const resumeReading = () => {
+    Speech.resume();
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ padding: 24 }}>
+      <TextInput
+        style={{ height: 200, borderColor: 'gray', borderWidth: 1 }}
+        multiline
+        onChangeText={setText}
+        value={text}
+      />
+      <Picker selectedValue={selectedVoice} onValueChange={(itemValue) => setSelectedVoice(itemValue)}>
+        {voices.map((voice, index) => <Picker.Item key={index} label={voice.name} value={voice.id} />)}
+      </Picker>
+      <Button title="Iniciar leitura" onPress={startReading} />
+      <Button title="Pausar leitura" onPress={pauseReading} />
+      <Button title="Retomar leitura" onPress={resumeReading} />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
